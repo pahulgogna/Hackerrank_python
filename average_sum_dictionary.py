3598daaa34b337c58b34ae8f714f11fbb6f81762
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2023-10-16 18:32:28 +0530

    solution

---
+++ average_sum_dictionary.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    sum = 0
+    nums = 0
+    for i in student_marks[query_name]:
+        sum += i
+        nums += 1
+    out = "{:.2f}".format((sum/nums))
+    print(out)
+    
